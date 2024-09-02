f402feeb89a4bdfca57626e52f2b9c044a08d86f
JoeyMucci <jmucci314@gmail.com>
2024-09-02 16:18:09 -0400

    instructions page

--- web/src/pages/InstructionsPage/InstructionsPage.jsx
+++ web/src/pages/InstructionsPage/InstructionsPage.jsx
@@ -1,20 +1,57 @@
-import { Link, routes } from '@redwoodjs/router'
-import { Metadata } from '@redwoodjs/web'
+import rules1 from 'web/public/rules1.png'
+import rules10 from 'web/public/rules10.png'
+import rules11 from 'web/public/rules11.png'
+import rules12 from 'web/public/rules12.png'
+import rules13 from 'web/public/rules13.png'
+import rules14 from 'web/public/rules14.png'
+import rules15 from 'web/public/rules15.png'
+import rules16 from 'web/public/rules16.png'
+import rules2 from 'web/public/rules2.png'
+import rules3 from 'web/public/rules3.png'
+import rules4 from 'web/public/rules4.png'
+import rules5 from 'web/public/rules5.png'
+import rules6 from 'web/public/rules6.png'
+import rules7 from 'web/public/rules7.png'
+import rules8 from 'web/public/rules8.png'
+import rules9 from 'web/public/rules9.png'
 
 const InstructionsPage = () => {
   return (
     <>
-      <Metadata title="Instructions" description="Instructions page" />
-
-      <h1>InstructionsPage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/InstructionsPage/InstructionsPage.jsx</code>
-      </p>
-      <p>
-        My default route is named <code>instructions</code>, link to me with `
-        <Link to={routes.instructions()}>Instructions</Link>`
-      </p>
+      <div className="flex flex-col">
+        <div className="flex flex-row">
+          <img className="basis-1/2" src={rules1} alt="An instuctions page" />
+          <img className="basis-1/2" src={rules2} alt="An instuctions page" />
+        </div>
+        <div className="flex h-screen flex-row">
+          <img className="basis-1/2" src={rules3} alt="An instuctions page" />
+          <img className="basis-1/2" src={rules4} alt="An instuctions page" />
+        </div>
+        <div className="flex h-screen flex-row">
+          <img className="basis-1/2" src={rules5} alt="An instuctions page" />
+          <img className="basis-1/2" src={rules6} alt="An instuctions page" />
+        </div>
+        <div className="flex h-screen flex-row">
+          <img className="basis-1/2" src={rules7} alt="An instuctions page" />
+          <img className="basis-1/2" src={rules8} alt="An instuctions page" />
+        </div>
+        <div className="flex h-screen flex-row">
+          <img className="basis-1/2" src={rules9} alt="An instuctions page" />
+          <img className="basis-1/2" src={rules10} alt="An instuctions page" />
+        </div>
+        <div className="flex h-screen flex-row">
+          <img className="basis-1/2" src={rules11} alt="An instuctions page" />
+          <img className="basis-1/2" src={rules12} alt="An instuctions page" />
+        </div>
+        <div className="flex h-screen flex-row">
+          <img className="basis-1/2" src={rules13} alt="An instuctions page" />
+          <img className="basis-1/2" src={rules14} alt="An instuctions page" />
+        </div>
+        <div className="flex h-screen flex-row">
+          <img className="basis-1/2" src={rules15} alt="An instuctions page" />
+          <img className="basis-1/2" src={rules16} alt="An instuctions page" />
+        </div>
+      </div>
     </>
   )
 }
